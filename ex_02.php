0457b95ff45360b309edc03425e95ab16c35cc8a
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-07-07 11:52:20 -0300

    add - ex1

---
+++ ex_02.php
@@ -0,0 +1,16 @@
+<!-- Exercício 02 – Espelho Mágico
+Uma empresa de tecnologia está desenvolvendo um sistema para tratamento de textos.
+Crie uma função chamada inverterTexto() que receba uma string e retorne o texto completamente invertido.
+Além disso, exiba a quantidade de caracteres existentes na string original. -->
+
+<?php 
+
+function inverterTexto(){
+
+    
+
+}
+
+$palavra = 'Brasil'
+
+?>
