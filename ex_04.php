8f43afdf9844f0a84e6adde9949f7ae89b71133c
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-07-08 12:51:32 -0300

    add- inicio ex4

---
+++ ex_04.php
@@ -0,0 +1,15 @@
+<!-- Exercício 04 – Gerador de Senhas
+Uma empresa deseja gerar senhas temporárias para seus colaboradores.
+Crie uma função chamada gerarSenha() que receba a quantidade de caracteres desejada e retorne uma senha aleatória contendo letras maiúsculas, minúsculas, números e caracteres especiais. -->
+
+<?php 
+
+function gerarSenha($numero){
+
+
+
+}
+
+$numero = 4;
+
+?>
