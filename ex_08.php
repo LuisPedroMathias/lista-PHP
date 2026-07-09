2cde357fcab3aa515c408b9a67f0b290b5e8b850
LuisPedroMathias <luis_mathias@estudante.sesisenai.org.br>
2026-07-09 12:56:35 -0300

    add - começo ex8

---
+++ ex_08.php
@@ -0,0 +1,16 @@
+<!-- Exercício 08 – Organizador de Lista
+Uma escola deseja organizar automaticamente a lista de alunos matriculados.
+Crie uma função chamada ordenarNomes() que receba uma string contendo nomes separados por vírgulas.
+A função deverá transformar os nomes em um vetor, remover espaços desnecessários, ordenar em ordem alfabética e retornar a lista organizada. -->
+
+<?php 
+
+function ordenarNomes($nomes){
+
+$vetor = explode(",",$nomes);
+
+}
+
+$nomes = 'Luis, Caio, Lucas, Maria, João, Elisa'
+
+?>
