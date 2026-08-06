f2ab014bc7e6912a366a32fad1bf66e5616a9d48
LuisPedroMathias <luis_mathias@estudante.sesisenai.ogr.br>
2026-08-06 08:49:22 -0300

    ADD - Finalizada ex_08

--- ex_08.php
+++ ex_08.php
@@ -3,14 +3,25 @@ Uma escola deseja organizar automaticamente a lista de alunos matriculados.
 Crie uma função chamada ordenarNomes() que receba uma string contendo nomes separados por vírgulas.
 A função deverá transformar os nomes em um vetor, remover espaços desnecessários, ordenar em ordem alfabética e retornar a lista organizada. -->
 
-<?php 
+<?php
 
-function ordenarNomes($nomes){
+function ordenarNomes($nomes) {
 
-$vetor = explode(",",$nomes);
+    $vetor = explode(",", $nomes);
 
+    $vetor = array_map('trim', $vetor);
+
+    sort($vetor);
+
+    return $vetor;
 }
 
-$nomes = 'Luis, Caio, Lucas, Maria, João, Elisa'
+$nomes = 'Luis, Caio, Lucas, Maria, João, Elisa';
+
+$listaOrganizada = ordenarNomes($nomes);
+
+foreach ($listaOrganizada as $nome) {
+    echo $nome . PHP_EOL;
+}
 
 ?>
